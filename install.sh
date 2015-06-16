94eb3d6ac69f335b635062f669bfeb8f88795b66
Fabian Lipp <fabian.lipp@uni-wuerzburg.de>
2015-06-16 18:26:36 +0200

    Check if repository file differs from local file in stall script

--- install.sh
+++ install.sh
@@ -9,19 +9,27 @@ if [ ! -e $BACKUP_DIR ]; then
 fi
 
 for file in $FILES; do
-	if [ -e ~/$file -a ! -L ~/$file ]; then
-		echo "Moving original file to $BACKUP_DIR$file"
-		SRC_DIR=$(dirname $file)
-		REL_DIR=${DIRNAME#~/}
-		if [ -n $REL_DIR ]; then
-			mkdir -p $BACKUP_DIR$REL_DIR
+	ORIG_FILE=~/$file
+	REPO_FILE=$DOTFILE_DIR/$file
+	BACKUP_FILE=$BACKUP_DIR$file
+	if [ -e $ORIG_FILE -a ! -L $ORIG_FILE ]; then
+		# Symlink for this file is not installed yet
+		if ! diff $ORIG_FILE $REPO_FILE > /dev/null; then
+			echo "File $file in repository differs from local file"
+			echo "Moving original file to $BACKUP_FILE"
 		fi
-		mv ~/$file $BACKUP_DIR$REL_DIR
+		# Write backup
+		SRC_DIR=$(dirname $file)
+		REL_SRC_DIR=${DIRNAME#~/}
+		if [ -n $REL_SRC_DIR ]; then
+			mkdir -p $BACKUP_DIR$REL_SRC_DIR
+		fi
+		mv $ORIG_FILE $BACKUP_DIR$REL_SRC_DIR
 	fi
 
-	if [ ! -e ~/$file ]; then
+	if [ ! -e $ORIG_FILE ]; then
 		echo "Installing symlink for $file"
-		ln -s $DOTFILE_DIR/$file ~/$file 
+		ln -s $REPO_FILE $ORIG_FILE
 	fi
 done
 

c5f71ca00993632568e6c8d0e7a5b79e2cbba018
Fabian Lipp <fabian.lipp@uni-wuerzburg.de>
2017-08-08 15:06:56 +0200

    Ipe configuration

---
+++ .ipe/ipelets/customize.lua
@@ -0,0 +1,11 @@
+----------------------------------------------------------------------
+---- My customization ipelet: customize.lua
+------------------------------------------------------------------------
+label = "My config customizations (Fabian Lipp)"
+
+about = [[
+Changes some settings (e. g. the external editor to gvim).
+]]
+
+prefs.external_editor = "gvim -f %s"
+
